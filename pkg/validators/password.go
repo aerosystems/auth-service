d4ca68823f198161095ce17806f80d5b91e5dfc2
artk0st <art_k0st@icloud.com>
2023-08-26 15:30:16 +0300

    update pass validator

--- pkg/validators/password.go
+++ pkg/validators/password.go
@@ -2,24 +2,15 @@ package validators
 
 import (
 	"errors"
-	"net/mail"
-	"os"
 	"regexp"
-	"strings"
 )
-
-func ValidateEmail(data string) (string, error) {
-	email, err := mail.ParseAddress(data)
-	if err != nil {
-		return "", err
-	}
-
-	return email.Address, nil
-}
 
 func ValidatePassword(password string) error {
 	if len(password) < 8 {
 		return errors.New("password should be of 8 characters long")
+	}
+	if len(password) > 128 {
+		return errors.New("password should not be of more than 128 characters long")
 	}
 	done, err := regexp.MatchString("([a-z])+", password)
 	if err != nil {
@@ -42,7 +33,6 @@ func ValidatePassword(password string) error {
 	if !done {
 		return errors.New("password should contain at least one digit")
 	}
-
 	done, err = regexp.MatchString("([!@#$%^&*.?-])+", password)
 	if err != nil {
 		return err
@@ -51,21 +41,4 @@ func ValidatePassword(password string) error {
 		return errors.New("password should contain at least one special character")
 	}
 	return nil
-}
-
-func ValidateRole(role string) error {
-	trustRoles := strings.Split(os.Getenv("TRUST_ROLES"), ",")
-	if !Contains(trustRoles, role) {
-		return errors.New("role exists in trusted roles")
-	}
-	return nil
-}
-
-func Contains(a []string, x string) bool {
-	for _, n := range a {
-		if x == n {
-			return true
-		}
-	}
-	return false
 }
